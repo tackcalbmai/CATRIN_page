655f04da5e98ea423d28c83d203b9e99043380ea
tackcalbmai <viktors.levdanskis@inbox.lv>
2026-08-04 15:12:16 +0300

    Rename homepage inspiration button in all languages

--- catrin-copy-final.js
+++ catrin-copy-final.js
@@ -1,3 +1,9 @@
 window.CATRIN_COPY = window.CATRIN_COPY || {};
 window.CATRIN_COPY.lv = window.CATRIN_COPY.lv || {};
+window.CATRIN_COPY.ru = window.CATRIN_COPY.ru || {};
+window.CATRIN_COPY.en = window.CATRIN_COPY.en || {};
+
 window.CATRIN_COPY.lv.homeTitle = "Kleita, kurā jūties īpaša";
+window.CATRIN_COPY.lv.seeDresses = "Iedvesmai";
+window.CATRIN_COPY.ru.seeDresses = "Для вдохновения";
+window.CATRIN_COPY.en.seeDresses = "For inspiration";
