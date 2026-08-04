8ccd2b4a516af893190a7ef13d92ec0943a4a69f
tackcalbmai <viktors.levdanskis@inbox.lv>
2026-08-04 15:23:57 +0300

    Add wedding dress storage service copy

--- catrin-copy-final.js
+++ catrin-copy-final.js
@@ -11,3 +11,14 @@ window.CATRIN_COPY.en.seeDresses = "For inspiration";
 window.CATRIN_COPY.lv.salonStoryAtelierText = "Šūšanu un pielāgošanu veic kvalificēta šuvēja. Salonā saskaņojam modeli, pielaikošanas un nepieciešamās izmaiņas, kā arī koordinējam darbu līdz gatavam rezultātam.";
 window.CATRIN_COPY.ru.salonStoryAtelierText = "Пошив и подгонку выполняет квалифицированная швея. В салоне мы согласуем модель, примерки и необходимые изменения и сопровождаем работу до готового результата.";
 window.CATRIN_COPY.en.salonStoryAtelierText = "Dressmaking and alterations are carried out by a qualified seamstress. At the salon, we agree the design, fittings and any required changes, and coordinate the work through to completion.";
+
+window.CATRIN_COPY.lv.salonLead = "Kopš 2011. gada palīdzam līgavām atrast, uzšūt un pielāgot kleitu viņu kāzu dienai. Salonā varat piemeklēt aksesuārus, vienoties par kleitas kopšanu un pēc iepriekšējas vienošanās atstāt to glabāšanā līdz kāzām.";
+window.CATRIN_COPY.ru.salonLead = "С 2011 года помогаем невестам найти, сшить и подогнать платье для свадебного дня. В салоне можно подобрать аксессуары, договориться об уходе за платьем и по предварительной договорённости оставить его на хранение до свадьбы.";
+window.CATRIN_COPY.en.salonLead = "Since 2011, we have helped brides find, create and alter dresses for their wedding day. At the salon, you can choose accessories, arrange dress care and, by prior agreement, leave the dress in storage until the wedding.";
+
+window.CATRIN_COPY.lv.serviceAccessoryTitle = "Aksesuāri un kleitas glabāšana";
+window.CATRIN_COPY.lv.serviceAccessoryText = "Piemeklējiet aksesuārus kopā ar kleitu. Pēc iepriekšējas vienošanās iegādāto kleitu varat atstāt glabāšanā līdz kāzām.";
+window.CATRIN_COPY.ru.serviceAccessoryTitle = "Аксессуары и хранение платья";
+window.CATRIN_COPY.ru.serviceAccessoryText = "Подберите аксессуары вместе с платьем. По предварительной договорённости купленное платье можно оставить на хранение до свадьбы.";
+window.CATRIN_COPY.en.serviceAccessoryTitle = "Accessories and dress storage";
+window.CATRIN_COPY.en.serviceAccessoryText = "Choose accessories together with the dress. By prior agreement, a purchased dress can be stored until the wedding.";
